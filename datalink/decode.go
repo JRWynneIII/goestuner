9a98a628b7e47f189d898788c7c52cf9d3125f9d
James Riley Wynne III <wynnejr@ornl.gov>
2025-09-28 18:21:15 -0400

    Restructured the decoder main loop to be more readable

--- datalink/decode.go
+++ datalink/decode.go
@@ -1,6 +1,7 @@
 package datalink
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/charmbracelet/log"
@@ -68,6 +69,10 @@ type Decoder struct {
 	AverageRsCorrections      float32
 	AvgVitCorrections         float32
 	SigQuality                float32
+
+	lastFrameOk         bool
+	recheckCounter      int
+	currentFrameCorrupt bool
 }
 
 func (d *Decoder) Close() {
@@ -117,6 +122,9 @@ func New(bufsize uint, configFile *koanf.Koanf) *Decoder {
 		RSParitySize:             32,
 		AverageRsCorrections:     0.0,
 		AvgVitCorrections:        0.0,
+		lastFrameOk:              false,
+		recheckCounter:           0,
+		currentFrameCorrupt:      false,
 	}
 
 	for i := 0; i < d.LastFrameSizeBits; i++ {
@@ -133,11 +141,120 @@ func New(bufsize uint, configFile *koanf.Koanf) *Decoder {
 	return &d
 }
 
-func (d *Decoder) Start() {
-	var isCorrupted bool
-	lastFrameOk := false
+func (d *Decoder) checkIfFrameLocked() {
+	// Use the correlator to see where the sync words are in the frame, such that we know where the packet starts
+	// If we're not frame locked, or we've gotten a lot of good packets and should make sure were on the right
+	// track and not out of sync, then try to recorrelate, otherwise, don't try and recorrelate the whole frame
+	if !d.lastFrameOk || d.recheckCounter >= d.MaxRecheckThreshold {
+		d.Correlator.Correlate(&d.EncodedBytes[0], uint(d.EncodedFrameSize))
+		d.recheckCounter = 0
+		d.lastFrameOk = false
+	} else {
+		//If we're already locked
+		d.Correlator.Correlate(&d.EncodedBytes[0], uint(d.EncodedFrameSize/64))
+		if d.Correlator.GetHighestCorrelationPosition() != 0 {
+			//Lost lock, so lets recorrelate the whole frame
+			d.Correlator.Correlate(&d.EncodedBytes[0], uint(d.EncodedFrameSize))
+			d.recheckCounter = 0
+		}
+	}
+	d.recheckCounter++
+}
 
-	recheckCounter := 0
+func (d *Decoder) correlate() error {
+	// Check to make sure we actually got enough data that contains a packet/frame
+	if correlation := d.Correlator.GetHighestCorrelation(); correlation < d.MinCorrelationBits {
+		log.Debugf("Correlation did not meet criteria: have: %d, want: %d", correlation, d.MinCorrelationBits)
+		d.lastFrameOk = false
+		return fmt.Errorf("No packet lock")
+	}
+
+	// Get the beginning of th epacket and shift things to start there
+	if pos := d.Correlator.GetHighestCorrelationPosition(); pos != 0 {
+		// Shift buffer to realign the frame to where the correlator says the frame begins
+		copy(d.EncodedBytes[:d.EncodedFrameSize-int(pos)], d.EncodedBytes[int(pos):d.EncodedFrameSize])
+
+		// Backfill bytes from the input channel to make a full frame
+		offset := uint(d.EncodedFrameSize) - pos
+		for i := offset; i < uint(d.EncodedFrameSize); i++ {
+			d.EncodedBytes[i] = <-d.SymbolsInput
+
+		}
+	}
+	return nil
+}
+
+func (d *Decoder) convolutionalDecode() {
+	// Prepend the remaining bits from last chunk to vit data so that the viterbi problem space is larger
+	// And therefore more likely to get a lock/decode
+	copy(d.ViterbiBytes[:d.LastFrameSizeBits], d.LastFrameEnd[:d.LastFrameSizeBits])
+	copy(d.ViterbiBytes[d.LastFrameSizeBits:], d.EncodedBytes[:d.EncodedFrameSize])
+
+	d.Viterbi.Decode(&d.ViterbiBytes[0], &d.DecodedBytes[0])
+
+}
+
+func (d *Decoder) calculateBitErrorRate() int {
+	// Track bit error rate
+	BER := d.Viterbi.GetBER() - (d.LastFrameSizeBits / 2)
+	if BER < 0 {
+		BER = 0
+	}
+	d.AvgVitCorrections += float32(BER)
+	return BER
+}
+
+func (d *Decoder) cleanFrame() {
+	// Shift the decoded bytes to cut out half of the 'last frame' bytes
+	// NOTE: not exactly sure why we do that, but it has something to do with incorporating the previous frame's
+	// 	 bytes into things
+	copy(d.DecodedBytes[:(d.FrameSize+d.LastFrameSizeBytes/2)-(d.LastFrameSizeBytes/2)], d.DecodedBytes[(d.LastFrameSizeBytes/2):(d.FrameSize+d.LastFrameSizeBytes/2)])
+
+	// Keep the last viterbi encoded bytes from this packet so that we can expand our problem space for the next
+	// packet's viterbi decode
+	// My understanding is this allows us to have a better chance at the viterbi decode working
+	copy(d.LastFrameEnd[:d.LastFrameSizeBits], d.ViterbiBytes[d.EncodedFrameSize:d.EncodedFrameSize+d.LastFrameSizeBits])
+	// Get the next sync words from the decoded frame.
+	copy(d.SyncWord[:d.SyncWordSize], d.DecodedBytes[:d.SyncWordSize])
+
+	// Shift the decoded bytes to remove the sync words, so we should just have a clean packet frame now
+	copy(d.DecodedBytes[:(d.FrameSize-d.SyncWordSize)-d.SyncWordSize], d.DecodedBytes[d.SyncWordSize:(d.FrameSize-d.SyncWordSize)])
+}
+
+func (d *Decoder) errorCorrectPacket() int32 {
+	//Reed Solomon Time
+	derrors := make([]int32, d.RsBlocks)
+	totalBytesFixed := int32(0)
+
+	for i := 0; i < int(d.RsBlocks); i++ {
+		d.ReedSolomon.Deinterleave(&d.DecodedBytes[0], &d.RSWorkBuffer[0], byte(i), d.RsBlocks)
+		derrors[i] = int32(int8(d.ReedSolomon.Decode_ccsds(&d.RSWorkBuffer[0])))
+
+		d.ReedSolomon.Interleave(&d.RSWorkBuffer[0], &d.RSCorrectedData[0], byte(i), d.RsBlocks)
+
+		if derrors[i] != -1 {
+			d.AverageRsCorrections += float32(derrors[i])
+		}
+		if derrors[i] > -1 {
+			totalBytesFixed += derrors[i]
+		}
+	}
+
+	if derrors[0] == -1 && derrors[1] == -1 && derrors[2] == -1 && derrors[3] == -1 {
+		// Packet is corrupt; :sadpanda:
+		d.currentFrameCorrupt = true
+		d.lastFrameOk = false
+	} else {
+		// Got a good packet! lets go!
+		d.currentFrameCorrupt = false
+		d.lastFrameOk = true
+	}
+
+	return totalBytesFixed
+
+}
+
+func (d *Decoder) Start() {
 	for {
 		//This is the meat and potatoes here. We should get our BER, SNR, and Sync status here
 		if len(d.SymbolsInput) >= d.EncodedFrameSize {
@@ -146,63 +263,24 @@ func (d *Decoder) Start() {
 				d.EncodedBytes[i] = <-d.SymbolsInput
 			}
 
-			// Use the correlator to see where the sync words are in the frame, such that we know where the packet starts
-			// If we're not frame locked, or we've gotten a lot of good packets and should make sure were on the right
-			// track and not out of sync, then try to recorrelate, otherwise, don't try and recorrelate the whole frame
-			if !lastFrameOk || recheckCounter >= d.MaxRecheckThreshold {
-				d.Correlator.Correlate(&d.EncodedBytes[0], uint(d.EncodedFrameSize))
-				recheckCounter = 0
-				lastFrameOk = false
-			} else {
-				//If we're already locked
-				d.Correlator.Correlate(&d.EncodedBytes[0], uint(d.EncodedFrameSize/64))
-				if d.Correlator.GetHighestCorrelationPosition() != 0 {
-					//Lost lock, so lets recorrelate the whole frame
-					d.Correlator.Correlate(&d.EncodedBytes[0], uint(d.EncodedFrameSize))
-					recheckCounter = 0
-				}
-			}
-			recheckCounter++
+			//Do we have frame sync?
+			d.checkIfFrameLocked()
 
-			// Check to make sure we actually got enough data that contains a packet/frame
-			if correlation := d.Correlator.GetHighestCorrelation(); correlation < d.MinCorrelationBits {
-				log.Debugf("Correlation did not meet criteria: have: %d, want: %d", correlation, d.MinCorrelationBits)
-				lastFrameOk = false
+			//Find beginning of frame
+			if err := d.correlate(); err != nil {
+				// If the correlation errored, we don't have a good frame, so skip to next iteration
 				continue
 			}
 
-			// Get the beginning of th epacket and shift things to start there
-			if pos := d.Correlator.GetHighestCorrelationPosition(); pos != 0 {
-				// Shift buffer to realign the frame to where the correlator says the frame begins
-				copy(d.EncodedBytes[:d.EncodedFrameSize-int(pos)], d.EncodedBytes[int(pos):d.EncodedFrameSize])
+			//Decode convolutional encoding
+			d.convolutionalDecode()
 
-				// Backfill bytes from the input channel to make a full frame
-				offset := uint(d.EncodedFrameSize) - pos
-				for i := offset; i < uint(d.EncodedFrameSize); i++ {
-					d.EncodedBytes[i] = <-d.SymbolsInput
-
-				}
-			}
-
-			// Prepend the remaining bits from last chunk to vit data so that the viterbi problem space is larger
-			// And therefore more likely to get a lock/decode
-			copy(d.ViterbiBytes[:d.LastFrameSizeBits], d.LastFrameEnd[:d.LastFrameSizeBits])
-			copy(d.ViterbiBytes[d.LastFrameSizeBits:], d.EncodedBytes[:d.EncodedFrameSize])
-
-			d.Viterbi.Decode(&d.ViterbiBytes[0], &d.DecodedBytes[0])
-
-			// Lets do our differential decode now
+			//Now lets do the differential decode
 			SatHelper.DifferentialEncodingNrzmDecode(&d.DecodedBytes[0], d.FrameSize+d.LastFrameSizeBytes)
 
-			// Track bit error rate
-			BER := d.Viterbi.GetBER() - (d.LastFrameSizeBits / 2)
-			if BER < 0 {
-				BER = 0
-			}
-			d.AvgVitCorrections += float32(BER)
+			BER := d.calculateBitErrorRate()
 
 			// Calculate our 'signal quality' percentage based upon the bit error rate
-			// TODO: Maybe we need to use the average viterbi corrections here instead of BER...
 			d.SigQuality = 100 * ((float32(d.MaxVitErrors) - float32(BER)) / float32(d.MaxVitErrors))
 			if d.SigQuality > 100 {
 				d.SigQuality = 100
@@ -210,53 +288,14 @@ func (d *Decoder) Start() {
 				d.SigQuality = 0
 			}
 
-			// Shift the decoded bytes to cut out half of the 'last frame' bytes
-			// NOTE: not exactly sure why we do that, but it has something to do with incorporating the previous frame's
-			// 	 bytes into things
-			copy(d.DecodedBytes[:(d.FrameSize+d.LastFrameSizeBytes/2)-(d.LastFrameSizeBytes/2)], d.DecodedBytes[(d.LastFrameSizeBytes/2):(d.FrameSize+d.LastFrameSizeBytes/2)])
+			d.cleanFrame()
 
-			// Keep the last viterbi encoded bytes from this packet so that we can expand our problem space for the next
-			// packet's viterbi decode
-			// My understanding is this allows us to have a better chance at the viterbi decode working
-			copy(d.LastFrameEnd[:d.LastFrameSizeBits], d.ViterbiBytes[d.EncodedFrameSize:d.EncodedFrameSize+d.LastFrameSizeBits])
-			// Get the next sync words from the decoded frame.
-			copy(d.SyncWord[:d.SyncWordSize], d.DecodedBytes[:d.SyncWordSize])
-
-			// Shift the decoded bytes to remove the sync words, so we should just have a clean packet frame now
-			copy(d.DecodedBytes[:(d.FrameSize-d.SyncWordSize)-d.SyncWordSize], d.DecodedBytes[d.SyncWordSize:(d.FrameSize-d.SyncWordSize)])
-
-			// NOTE: I'm honestly not sure what this does, but its important...
+			// Derandomize packet: Unsure what exactly this does tbh
 			SatHelper.DeRandomizerDeRandomize(&d.DecodedBytes[0], d.FrameSize-d.SyncWordSize)
 
-			//Reed Solomon Time
-			derrors := make([]int32, d.RsBlocks)
-			totalBytesFixed := int32(0)
-
-			for i := 0; i < int(d.RsBlocks); i++ {
-				d.ReedSolomon.Deinterleave(&d.DecodedBytes[0], &d.RSWorkBuffer[0], byte(i), d.RsBlocks)
-				derrors[i] = int32(int8(d.ReedSolomon.Decode_ccsds(&d.RSWorkBuffer[0])))
-
-				d.ReedSolomon.Interleave(&d.RSWorkBuffer[0], &d.RSCorrectedData[0], byte(i), d.RsBlocks)
-
-				if derrors[i] != -1 {
-					d.AverageRsCorrections += float32(derrors[i])
-				}
-				if derrors[i] > -1 {
-					totalBytesFixed += derrors[i]
-				}
-			}
+			totalBytesFixed := d.errorCorrectPacket()
 
 			d.TotalFramesProcessed++
-
-			if derrors[0] == -1 && derrors[1] == -1 && derrors[2] == -1 && derrors[3] == -1 {
-				// Packet is corrupt; :sadpanda:
-				isCorrupted = true
-				lastFrameOk = false
-			} else {
-				// Got a good packet! lets go!
-				isCorrupted = false
-				lastFrameOk = true
-			}
 
 			// Spacecraft ID (TODO: This seems to always be 0 for some reason?)
 			scid := ((d.RSCorrectedData[0] & 0x3F) << 2) | (d.RSCorrectedData[1]&0xC0)>>6
@@ -269,7 +308,7 @@ func (d *Decoder) Start() {
 			counter &= 0xFFFFFF00
 			counter >>= 8
 
-			if !isCorrupted {
+			if !d.currentFrameCorrupt {
 				d.FrameLock = true
 				log.Infof("Got frame: vcid: %d (%s) scid: %d object number: %d", int(vcid), VCIDs[int(vcid)], scid, counter)
 				if totalBytesFixed > 0 {
